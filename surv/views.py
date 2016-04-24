a8e33fb6f98912c8d689a14a83995ab9fff58b02
Sujit Maharjan <sujitmhj@gmail.com>
2016-04-24 08:20:57 +0545

    changes

--- surv/views.py
+++ surv/views.py
@@ -3,13 +3,13 @@ from django.views.generic import View
 
 from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
 class home(View):
 
     def get(self, request, *args, **kwargs):
     	print request.GET
         return HttpResponse('Hello, World!')
 
+    @method_decorator(csrf_exempt)
     def post(self, request, *args, **kwargs):
         print request.POST
         return HttpResponse('Hello, World!')
