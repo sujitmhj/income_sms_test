b3262389827ff9428e9c43c32dd680005e7846b6
Sujit Maharjan <sujitmhj@gmail.com>
2016-04-24 08:24:01 +0545

    changes

--- surv/views.py
+++ surv/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from django.views.generic import View
-
+from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 class home(View):
