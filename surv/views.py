9f962c799d16b43fc2dcd10472c479d083e80100
Sujit Maharjan <sujitmhj@gmail.com>
2016-04-24 08:28:59 +0545

    changes

--- surv/views.py
+++ surv/views.py
@@ -4,12 +4,15 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 class home(View):
+	@method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super(home, self).dispatch(request, *args, **kwargs)
+
 
     def get(self, request, *args, **kwargs):
     	print request.GET
         return HttpResponse('Hello, World!')
 
-    @method_decorator(csrf_exempt)
     def post(self, request, *args, **kwargs):
         print request.POST
         return HttpResponse('Hello, World!')
